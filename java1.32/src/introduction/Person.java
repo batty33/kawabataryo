1b866f9305b1a3a05a889bb2fb3b47566c9960f1
batty33 <kuraisisuxu@gmail.com>
2023-08-13 02:53:32 +0900

    課題1.32提出

---
+++ java1.32/src/introduction/Person.java
@@ -0,0 +1,36 @@
+package introduction;
+
+class Person {
+	public String name;
+	public int age;
+	public double height;
+	public double weight;
+	// 問1 カウントの宣言
+	public static int count = 0;
+
+	Person(String name, int age, double height, double weight) {
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		
+		// 問2 カウントを増やす
+		count ++;
+	}
+
+	public double bmi() {
+		return this.weight / this.height / this.height;
+	}
+
+	public void print() {
+		System.out.println("名前は" + this.name + "です");
+		System.out.println("年は" + this.age + "です");
+	}
+	
+	// 問4 クラスメソッド生成
+	public static void printCount() {
+		
+		// 問5 コンソールへ出力
+		System.out.println("合計" + count +"円です");
+	}
+}
